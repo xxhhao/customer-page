5d79a833dac32687a011406dcacd3cc3276ba4f3
xh <xxhhao@126.com>
2017-09-19 17:01:06 +0800

    notebook

--- js/index.js
+++ js/index.js
@@ -1,19 +1,65 @@
 
+
 $(function(){
-    $("#name").blur(function(){
-        check()
-    })
+    $("#confirm").click(function(){
+        name();
+        realname();
+        telphone();
+    });
+
+    $(".check").blur(function(){
+        name();
+    });
+
 });
 
 
-    function check(){
-        var ch1=$("#name").val();
-        if(ch1==""){
-            $("#name").parent().addClass("has-error");
-            $("#name").siblings().show();
+    function name(){
+        var ch1=$("#name");
+        if(ch1.val()==""){
+            ch1.parent().addClass("has-error");
+            ch1.next().show();
+        }
+        else if(ch1.val().length<=6) {
+            ch1.parent().removeClass("has-error");
+            ch1.next().hide();
+        }
+        else if(ch1.val().length>6){
+            ch1.parent().addClass("has-error");
+            ch1.next().show();
+            ch1.next().text("²»ÄÜ");
+        }
+
+        //ch1.blur(function(){
+        //    if(ch1.val()!==""){
+        //        ch1.parent().removeClass("has-error");
+        //        ch1.siblings().hide();
+        //    }
+        //})
+
+    }
+
+
+    function realname(){
+        var ch1=$("#realname");
+        if(ch1.val()==""){
+            ch1.parent().addClass("has-error");
+            ch1.siblings().show();
         }
         else{
-            $("#name").parent().removeClass("has-error")
-            $("#name").siblings().hide();
+            ch1.parent().removeClass("has-error");
+            ch1.siblings().hide();
         }
-    };
+}
+
+    function telphone(){
+        var ch1=$("#telphone")
+        if(ch1.val()==""||ch1.val().length!=11){
+            ch1.parent().addClass("has-error");
+            ch1.siblings().show();
+        }
+        else{
+            ch1.parent().removeClass("has-error");
+            ch1.siblings().hide();
+                }
+    }
